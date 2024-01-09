e51440ec8b139aff011f17edcd57712cec161076
harshitclub <harshitclub@gmail.com>
2024-01-09 11:54:49 +0530

    frontend

---
+++ client/src/app/admin/dashboard/notes/page.jsx
@@ -0,0 +1,190 @@
+import React from "react";
+import "./style.css";
+import { RiDeleteBin5Line, RiEdit2Fill } from "react-icons/ri";
+
+function page() {
+  return (
+    <main className="adminNotes width100 flex flexColumn">
+      <section className="adminNotesContainer width100 flex justifyStart">
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+        <div className="adminNote flex flexColumn spaceBtw">
+          <div className="adminNoteHead flex width100 justifyEnd">
+            <p>Mark as done</p>
+          </div>
+          <div className="adminNoteContent width100">
+            <p>Fold laundry while watching a show.</p>
+          </div>
+          <div className="adminNoteActions width100 flex spaceBtw">
+            <button className="adminNoteEdit">
+              Edit <RiEdit2Fill style={{ marginBottom: "-2px" }} />
+            </button>
+            <button className="adminNoteDelete">
+              <RiDeleteBin5Line style={{ marginBottom: "-2px" }} />
+            </button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default page;
